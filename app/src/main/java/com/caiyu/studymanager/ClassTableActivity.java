f7649a930d0bbc5d0601fb7545f69f416ef3aef3
zhinengfeiyu <yucaidongzhou@gmail.com>
2016-01-23 20:54:59 +0800

    登录和注册界面

---
+++ app/src/main/java/com/caiyu/studymanager/ClassTableActivity.java
@@ -0,0 +1,16 @@
+package com.caiyu.studymanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 渝 on 2016/1/23.
+ */
+public class ClassTableActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_table);
+    }
+}
